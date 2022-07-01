7a572dfb287287c18db9e9f68fc4b56df85594c2
Caio Tavares <caio.tavares11@gmail.com>
2022-07-01 18:17:15 -0300

    WIP

--- src/clj_stack/core.clj
+++ src/clj_stack/core.clj
@@ -1,25 +1,37 @@
 (ns clj-stack.core
-  (:use [criterium.core]))
+  (:require [clojure.pprint :as pprint]))
 
-(defmacro with-probe [& forms]
-  `())
+(defmacro defn [& fn]
+  `(clojure.core/defn ~@fn))
+
+(def *args* (atom {}))
 
 (defn function-2 [args]
   {:status 200 :body args})
 
 (defn function-1-1 [args]
-  (+ 1 1))
+  (let [fn-symbol (-> (Thread/currentThread)
+                      .getStackTrace
+                      second
+                      .getClassName
+                      clojure.repl/demunge
+                      symbol)
+        arglist (-> fn-symbol find-var meta :arglists)]
+    (swap! *args* merge {(keyword fn-symbol) {:arglist       arglist
+                                              :provided-args [args]}})
+    (+ 1 1)))
+
+
+(function-1-1 {:banana 1})
+
+(@*args*)
+
+;(macroexpand-1 '(with-probe))
+
 
 (defn function-1 [args]
   (let [opa (function-1-1 {:args1-1 args})]
-    (mapv #(clojure.pprint/pprint (.toString %)) (.getStackTrace (Thread/currentThread)))
+    (mapv #(print (.toString %)) (.getStackTrace (Thread/currentThread)))
     (function-2 {:args2 opa})))
 
-(clojure.core/tap-loop)
-
 (function-1 {:banana 1})
-
-(def ^Thread thread (Thread/currentThread))
-
-
-(def  stacktrace (.getStackTrace thread))
