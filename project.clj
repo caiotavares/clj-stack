ed2914fe5a3935dd391c2e4ef7f063d5bc3f4c69
Caio Tavares <caio.tavares11@gmail.com>
2022-09-27 11:29:35 -0300

    Update project name to deploy to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stack "0.1.0-SNAPSHOT"
+(defproject org.clojars.caiotavares/clj-stack "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
